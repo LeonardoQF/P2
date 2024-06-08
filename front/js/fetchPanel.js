ac366fecc1d06c72a5d3ddbe625ff1414fc9c13e
Bioxcis-dono <102638604+Bioxcis@users.noreply.github.com>
2024-06-08 19:31:38 -0300

    Update Panel

---
+++ front/js/fetchPanel.js
@@ -0,0 +1,38 @@
+let itemCount = 0;
+
+function addItem() {
+    let dataPokemon = localStorage.getItem('currentPokemon');
+    if (typeof dataPokemon === 'string') {
+        dataPokemon = JSON.parse(dataPokemon);
+    }
+
+    itemCount++;
+    const panel = document.getElementById('scrollablePanel');
+    const newItem = document.createElement('div');
+    newItem.className = 'item';
+    newItem.innerHTML = `
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png" alt="mewtwo">
+        <div class="itens-info">
+            <h3>mewtwo</h3>
+            <p><strong>HP:</strong>106</p>
+            <p><strong>Attack:</strong>110</p>
+            <p><strong>Defense:</strong>90</p>
+            <p><strong>Speed:</strong>130</p>
+            <p><strong>Special Attack:</strong>154</p>
+            <p><strong>Special Defense:</strong>90</p>
+            <p><strong>Height:</strong>20</p>
+            <p><strong>Weight:</strong>1220</p>
+        </div>
+        <button class="remove-button" onclick="removeItem(this)">Remover</button>
+    `;
+    panel.appendChild(newItem);
+}
+
+function removeItem(button) {
+    const item = button.parentElement;
+    item.remove();
+}
+
+addItem()
+addItem()
+addItem()
